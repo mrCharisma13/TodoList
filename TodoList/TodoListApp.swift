611f8bfa86f9652e1a92da3e816cb4242cd105a7
mrCharisma13 <115006868+mrCharisma13@users.noreply.github.com>
2022-10-09 14:57:53 +0300

    landscape & dark mode

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -13,7 +13,7 @@ struct TodoListApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            NavigationStack {
                 ListView()
             }
             .environmentObject(listViewModel)
